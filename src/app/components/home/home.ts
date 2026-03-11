f73926794cdd59bfec0caa48b3e6eb8a0a6926ff
UBSITMarianoKyle <20243359@s.ubaguio.edu>
2026-03-11 22:28:39 +0800

    11/03/2026

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 @Component({
@@ -7,4 +7,44 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './home.html',
   styleUrls: ['./home.css']  // fixed typo
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+
+  studentName = "Mariano, Kyle";
+  school = "University of Baguio";
+  subject = "Application Development 1";
+
+  typedText = "";
+  textArray = [
+    "Future Software Developer 💻",
+    "Professional Bug Creator 🐛",
+    "Angular Student Developer ⚡",
+    "Still Googling Errors Since 2024 😂"
+  ];
+
+  textIndex = 0;
+  charIndex = 0;
+
+  ngOnInit(){
+    this.typeEffect();
+  }
+
+  typeEffect(){
+    const currentText = this.textArray[this.textIndex];
+
+    if(this.charIndex < currentText.length){
+      this.typedText += currentText.charAt(this.charIndex);
+      this.charIndex++;
+
+      setTimeout(()=>this.typeEffect(),80);
+    }
+    else{
+      setTimeout(()=>{
+        this.typedText="";
+        this.charIndex=0;
+        this.textIndex=(this.textIndex+1)%this.textArray.length;
+        this.typeEffect();
+      },2000);
+    }
+  }
+
+}
